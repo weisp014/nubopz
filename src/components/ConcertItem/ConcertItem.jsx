6b7dddc0cd22fac52fcfbeafbe2ba76ff75bad33
Ryan Weispfenning <weisp014@gmail.com>
2023-04-15 20:55:02 -0500

    added save button to details page to enable user to save concert info

--- src/components/ConcertItem/ConcertItem.jsx
+++ src/components/ConcertItem/ConcertItem.jsx
@@ -19,7 +19,7 @@ function ConcertItem({ concert }) {
       <img src={concert.images[0].url} width={200} />
       <h2>{concert.name}</h2>
       <p>{concert._embedded && concert._embedded.venues[0].name}</p>
-      {/* <p>Price:{concert.priceRanges && concert.priceRanges[0].min}</p> */}
+      {/* <p>Price:{concert.priceRanges.length && concert.priceRanges[0].min}</p> */}
       <h3>{concert.dates.start.localDate}</h3>
       <button onClick={handleDetails}>Details</button>
     </div>
--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 function DetailsPage() {
+  const dispatch = useDispatch();
   const concertDetails = useSelector((store) => store.details);
+  console.log(concertDetails);
+
+  // TODO: Add info and price?
+  const saveConcert = () => {
+    dispatch({
+      type: "SAVE_CONCERT",
+      payload: {
+        event_id: concertDetails.id,
+        event_name: concertDetails.name,
+        venue: concertDetails._embedded.venues[0].name,
+        image_url: concertDetails.images[0].url,
+        date: concertDetails.dates.start.dateTime,
+      },
+    });
+  };
 
   return (
     <>
       {concertDetails?.name && (
         <div>
-          <img src={concertDetails.images[0].url} width={200} />
+          <img src={concertDetails.images[0].url} width={300} />
           <h2>{concertDetails.name}</h2>
           <p>
-            {concertDetails._embedded && concertDetails._embedded.venues[0].name}
+            {concertDetails._embedded &&
+              concertDetails._embedded.venues[0].name}
           </p>
-          <h3>{concertDetails.dates.start.localDate}</h3>
+          <h3>{concertDetails.dates.start.dateTime}</h3>
+          <button onClick={saveConcert}>SAVE</button>
         </div>
       )}
     </>
