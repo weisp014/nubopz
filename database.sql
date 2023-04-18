6ebd9a849310ab5f43196d1d665f9fffa354b166
Ryan Weispfenning <weisp014@gmail.com>
2023-04-18 13:57:43 -0500

    added query to create "favorites" table

--- database.sql
+++ database.sql
@@ -8,3 +8,20 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "favorites" (
+	"id" serial NOT NULL,
+	"user_id" INT REFERENCES "user",
+	"event_id" varchar(255) NOT NULL UNIQUE,
+	"event_name" varchar(255) NOT NULL,
+	"venue" varchar(255) NOT NULL,
+	"image_url" varchar(255) NOT NULL,
+	"date" DATE NOT NULL,
+	"info" varchar(255),
+	"price" varchar(255),
+	"attended" BOOLEAN DEFAULT 'false'
+);
+
+--unique index ensures a user can't add same event more than once
+CREATE UNIQUE INDEX ind_user_event_id
+ON "favorites" ("user_id", "event_id");
