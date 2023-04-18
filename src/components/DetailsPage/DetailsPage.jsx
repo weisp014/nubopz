83e7794c1a61a677d93e11b63be70cf66312b219
Ryan Weispfenning <weisp014@gmail.com>
2023-04-18 14:46:18 -0500

    added snackbar to show concert saved

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 function DetailsPage() {
   const dispatch = useDispatch();
   // get details from the store
   const concertDetails = useSelector((store) => store.details);
 
-  // local state to control save button
+  // state to control save button
   const [saveToggle, setSaveToggle] = useState(true);
+  // state to control snackbar
+  const [open, setOpen] = useState(false);
+  // custom alert component from MUI
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
 
   // TODO: Add info and price?
   const saveConcert = () => {
     // changing toggle to remove save button
     setSaveToggle(false);
+    // show snackbar
+    setOpen(true);
     dispatch({
       type: "SAVE_CONCERT",
       payload: {
@@ -20,7 +30,8 @@ function DetailsPage() {
         event_name: concertDetails.name,
         venue: concertDetails._embedded.venues[0].name,
         image_url: concertDetails.images[0].url,
-        date: concertDetails.dates.start.dateTime,
+        date: concertDetails.dates.start.localDate,
+        time: concertDetails.dates.start.localTime,
       },
     });
   };
@@ -28,19 +39,30 @@ function DetailsPage() {
   return (
     <>
       {concertDetails?.name && (
-        <div>
-          <img src={concertDetails.images[0].url} width={300} />
-          <h2>{concertDetails.name}</h2>
-          <p>
-            {concertDetails._embedded &&
-              concertDetails._embedded.venues[0].name}
-          </p>
-          <h3>{concertDetails.dates.start.dateTime}</h3>
-          {/* show save button if saveToggle true */}
-          {saveToggle && <button onClick={saveConcert}>SAVE</button>
-          }
-          
-        </div>
+        <center>
+          <div>
+            <img src={concertDetails.images[3].url} width={300} />
+            <h2>{concertDetails.name}</h2>
+            <p>
+              {concertDetails._embedded.venues[0].name},{" "}
+              {concertDetails._embedded.venues[0].city.name}
+            </p>
+            <h3>
+              {concertDetails.dates.start.localDate}{" "}
+              {concertDetails.dates.start.localTime}
+            </h3>
+            {/* show save button if saveToggle true and show snackbar after clicking SAVE */}
+            {saveToggle && <button onClick={saveConcert}>SAVE</button>}
+            <Snackbar open={open} anchorOrigin={{vertical: "top", horizontal: "center"}} autoHideDuration={2000} onClose={() => setOpen(false)}>
+              <Alert
+                severity="success"
+                sx={{ width: "100%" }}
+              >
+                Concert Saved!
+              </Alert>
+            </Snackbar>
+          </div>
+        </center>
       )}
     </>
   );
