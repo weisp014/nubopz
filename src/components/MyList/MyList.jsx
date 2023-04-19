2c5dde1090feda7a71cefd223b7868b9a461c099
Ryan Weispfenning <weisp014@gmail.com>
2023-04-18 21:09:33 -0500

    added toggle button to filter concerts by attended

--- src/components/MyList/MyList.jsx
+++ src/components/MyList/MyList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Grid from "@mui/material/Grid";
+import { Grid, ToggleButton } from "@mui/material";
 
 import SavedConcertItem from "../SavedConcertItem/SavedConcertItem";
 
@@ -8,24 +8,45 @@ function MyList() {
   const dispatch = useDispatch();
   // get saved concerts from the store
   const savedConcerts = useSelector((store) => store.favorites);
-  console.log(savedConcerts);
+  // state to track filter selection
+  const [attendedFilter, setAttendedFilter] = useState(false);
   // on page load fetch user's saved concerts
   useEffect(() => {
     dispatch({
       type: "FETCH_MY_CONCERTS",
+      payload: {
+        attendedFilter,
+      },
     });
-  }, []);
+  }, [attendedFilter]);
 
   return (
     <>
-    <center>
-    <Grid container spacing={{ xs: 2, md: 3 }}>
-      {savedConcerts.length &&
-        savedConcerts.map((concert) => (
-          <SavedConcertItem key={concert.id} concert={concert} />
-        ))}
-    </Grid>
-    </center>
+      <center>
+        {/* toggle showing attended concerts */}
+        <ToggleButton
+          sx={{ margin: "10px" }}
+          value="attended"
+          selected={attendedFilter}
+          onChange={() => {
+            setAttendedFilter(!attendedFilter);
+          }}
+        >
+          Attended
+        </ToggleButton>
+      </center>
+      <center>
+        <Grid container spacing={{ xs: 2, md: 3 }}>
+          {savedConcerts.length &&
+            savedConcerts.map((concert) => (
+              <SavedConcertItem
+                key={concert.id}
+                concert={concert}
+                attendedFilter={attendedFilter}
+              />
+            ))}
+        </Grid>
+      </center>
     </>
   );
 }
