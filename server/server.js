557269fd1f5c496aad3e149a3bdddbbc3f0f7f92
Ryan Weispfenning <weisp014@gmail.com>
2023-04-20 10:55:39 -0500

    removed spotify strategy using passport. not working

--- server/server.js
+++ server/server.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-//! added cors
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+// added for spotify use
+const cookieParser = require("cookie-parser");
 const cors = require('cors');
 
 const app = express();
 
-const sessionMiddleware = require('./modules/session-middleware');
-const passport = require('./strategies/user.strategy');
-// !do I need a separate passport2?
-const passport2 = require('./strategies/spotify.strategy');
+const sessionMiddleware = require("./modules/session-middleware");
+const passport = require("./strategies/user.strategy");
 
 // Route includes
-const userRouter = require('./routes/user.router');
-const concertsRouter = require('./routes/concerts.router');
-const spotifyRouter = require('./routes/spotify.router');
+const userRouter = require("./routes/user.router");
+const concertsRouter = require("./routes/concerts.router");
+const spotifyRouter = require("./routes/spotify.router");
 
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+app.use(cookieParser())
 
 // Passport Session Configuration //
 app.use(sessionMiddleware);
@@ -27,19 +29,13 @@ app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
 
-// !start up passport sessions for spotify
-app.use(passport2.initialize());
-app.use(passport2.session());
-
 /* Routes */
-app.use('/api/user', userRouter);
-app.use('/api/concerts', concertsRouter);
-app.use('/api/spotify', spotifyRouter);
+app.use("/api/user", userRouter);
+app.use("/api/concerts", concertsRouter);
+app.use("/api/spotify", spotifyRouter);
 
-//! added cors
 // Serve static files
-app.use(express.static('build'))
-app.use(cors());
+app.use(express.static("build"))
 
 // App Set //
 const PORT = process.env.PORT || 5000;
