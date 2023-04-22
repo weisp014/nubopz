fe7d8afd05adcc5851ca2b2c6fb37397ad58c4ae
Ryan Weispfenning <weisp014@gmail.com>
2023-04-21 20:53:22 -0500

    added list of top tracks with links on details page

---
+++ src/redux/reducers/tracks.reducer.js
@@ -0,0 +1,8 @@
+const tracks = (state=[], action) => {
+    if (action.type === "SET_TOP_TRACKS") {
+        return action.payload;
+    }
+    return state;
+}
+
+export default tracks;
--- src/redux/sagas/spotify.saga.js
+++ src/redux/sagas/spotify.saga.js
@@ -3,16 +3,21 @@ import { put, takeLatest } from "redux-saga/effects";
 
 // first GET request to spotify to get artist ID for the artist on the details page
 // 2nd GET request with the artist ID to get top tracks for that artist
-function* getTracks() {
+function* getTracks(action) {
   try {
-    const artist = yield axios.get("/api/spotify/artist");
+    console.log("artist name:", action.payload);
+    const artist = yield axios.get(`/api/spotify/artist/${action.payload}`);
     console.log(artist.data.artists.items[0].id);
     const tracks = yield axios.get("/api/spotify/tracks", {
       params: {
-        artist: artist.data.artists.items[0].id
+        artistID: artist.data.artists.items[0].id
       },
     });
-    console.log('incoming songs:', tracks);
+    console.log('incoming tracks:', tracks.data.tracks);
+    yield put({
+        type: "SET_TOP_TRACKS",
+        payload: tracks.data.tracks
+    })
   } catch (err) {
     console.log("error getting spotify:", err);
   }
