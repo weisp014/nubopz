a169c5f070349ad83b89a3822385c66917a4f038
Ryan Weispfenning <weisp014@gmail.com>
2023-04-23 16:06:19 -0500

    added search box on my list page for saved concerts and conditional rendering

--- src/components/MyList/MyList.jsx
+++ src/components/MyList/MyList.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Grid, ToggleButton } from "@mui/material";
+import { Grid, ToggleButton, Stack } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
 import SavedConcertItem from "../SavedConcertItem/SavedConcertItem";
 
@@ -8,8 +10,12 @@ function MyList() {
   const dispatch = useDispatch();
   // get saved concerts from the store
   const savedConcerts = useSelector((store) => store.favorites);
+  // creating new array of only concert names from list of saved concerts as options for search input
+  const searchConcerts = savedConcerts.map((concert) => concert.event_name);
   // state to track filter selection
   const [attendedFilter, setAttendedFilter] = useState(false);
+  // state to track name entered in search input
+  const [searchValue, setSearchValue] = useState(null);
   // on page load fetch user's saved concerts
   useEffect(() => {
     dispatch({
@@ -23,33 +29,69 @@ function MyList() {
   return (
     <>
       <center>
-        {/* toggle showing attended concerts */}
-        <ToggleButton
-          sx={{ margin: "10px" }}
-          value="attended"
-          selected={attendedFilter}
-          onChange={() => {
-            setAttendedFilter(!attendedFilter);
-          }}
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+          spacing={2}
+          sx={{ mb: "30px" }}
         >
-          Attended
-        </ToggleButton>
+          {/* toggle showing attended concerts */}
+          <ToggleButton
+            value="attended"
+            color="primary"
+            selected={attendedFilter}
+            onChange={() => {
+              setAttendedFilter(!attendedFilter);
+            }}
+          >
+            Attended
+          </ToggleButton>
+          {/* Search field for saved concerts */}
+          <Autocomplete
+            disablePortal
+            value={searchValue}
+            onChange={(event, newSearchValue) => {
+              setSearchValue(newSearchValue);
+            }}
+            id="concert-search"
+            options={searchConcerts}
+            sx={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Search for Concert" />
+            )}
+          />
+        </Stack>
       </center>
       <center>
-        {savedConcerts.length ? (
-          <Grid container spacing={{ xs: 2, md: 3 }}>
-            {savedConcerts.length &&
+        <Grid container spacing={{ xs: 2, md: 3 }}>
+          {/* Check if search input is null (not entered),
+           true: map out list of concerts
+           false: only map out concerts where the name matches the search input value */}
+          {searchValue === null ? (
+            savedConcerts.length ? (
               savedConcerts.map((concert) => (
                 <SavedConcertItem
                   key={concert.id}
                   concert={concert}
                   attendedFilter={attendedFilter}
                 />
-              ))}
-          </Grid>
-        ) : (
-          <h2>No concerts to display</h2>
-        )}
+              ))
+            ) : (
+              <h2>No concerts to display</h2>
+            )
+          ) : (
+            savedConcerts.filter(
+              (concert) => concert.event_name === searchValue
+            ).map((concert) => (
+              <SavedConcertItem
+                key={concert.id}
+                concert={concert}
+                attendedFilter={attendedFilter}
+              />
+            ))
+          )}
+        </Grid>
       </center>
     </>
   );
